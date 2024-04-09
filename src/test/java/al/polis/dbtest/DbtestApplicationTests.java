d4a2b61fcac5c3526e7c0a7088aa4de4a6ca3746
ceda-dev-01 <l.lezzerini@coopceda.it>
2024-04-09 13:26:35 +0200

    First commit after Spring Initializr

---
+++ src/test/java/al/polis/dbtest/DbtestApplicationTests.java
@@ -0,0 +1,13 @@
+package al.polis.dbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
